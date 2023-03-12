4d6713d0f7557c8a84e8e4e0c9603274d9015fcd
zywang <zywang9710@gmail.com>
2023-03-12 15:56:58 -0400

    test reentrant lock

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,5 @@ import myconcurrent.ReentrantLock;
 import static java.lang.Thread.sleep;
 
 public class Main {
-    public static void main(String[] args) {
-        TestReentrantLock test1 = new TestReentrantLock();
-        test1.doTest();
-    }
-}
-
-class TestReentrantLock {
-    int a = 0;
-
-    public void doTest() {
-        ReentrantLock lock = new ReentrantLock();
-
-
-        Thread t1 = new Thread(() -> adding(lock));
-        Thread t2 = new Thread(() -> adding(lock));
-
-        t1.start();
-        t2.start();
-
-        try {
-            sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        System.out.println(a);
-    }
-
-    private void adding(ReentrantLock lock) {
-        for (int i = 0; i < 100; i++) {
-            lock.lock();
-            System.out.println(Thread.currentThread() + " get the lock");
-            a += 1;
-            lock.unlock();
-            System.out.println(Thread.currentThread() + " release the lock");
-        }
-    }
+    public static void main(String[] args) { System.out.println("Hello world!"); }
 }
---
+++ src/test/ReentrantLockTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import myconcurrent.ReentrantLock;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Thread.sleep;
+
+class ReentrantLockTest {
+    int a = 0;
+
+    @Test
+    public void doTest() {
+        ReentrantLock lock = new ReentrantLock();
+
+        Thread t1 = new Thread(() -> adding(lock));
+        Thread t2 = new Thread(() -> adding(lock));
+        Thread t3 = new Thread(() -> adding(lock));
+        Thread t4 = new Thread(() -> adding(lock));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        assert a == 400: "a = " + a + ", Test failed.";
+        System.out.println("a == " + a);
+    }
+
+    private void adding(ReentrantLock lock) {
+        for (int i = 0; i < 100; i++) {
+            lock.lock();
+            System.out.println(Thread.currentThread() + " get the lock");
+            a += 1;
+            lock.unlock();
+            System.out.println(Thread.currentThread() + " release the lock");
+        }
+    }
+}
